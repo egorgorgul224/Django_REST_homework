31cc268bf62d3022938b78ee037ebcd253812e16
egor <scudf24@gmail.com>
2025-08-01 22:25:54 +0300

    completed the third task(added new field to display a list of lessons)

--- materials/serializers.py
+++ materials/serializers.py
@@ -3,10 +3,19 @@ from rest_framework import serializers
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    """Сериализация модели Lesson. Используются все поля."""
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(serializers.ModelSerializer):
     """Сериализация модели Course. Используются все поля."""
 
     lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     def get_lesson_count(self, obj):
         """Метод для подсчета количества уроков в курсе. 'lessons' - related_name поля 'course' модели Lesson."""
@@ -15,12 +24,4 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = "__all__"
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    """Сериализация модели Lesson. Используются все поля."""
-
-    class Meta:
-        model = Lesson
         fields = "__all__"
